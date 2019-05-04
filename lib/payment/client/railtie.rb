b00f18621013494f5677705a1b128be4f7fd9548
akira noguchi <guppy0356.nakira@gmail.com>
2019-05-04 12:14:24 +0900

    Bugfix use ActiveSupport::OrderedOptions

--- lib/payment/client/railtie.rb
+++ lib/payment/client/railtie.rb
@@ -2,7 +2,7 @@ require 'rails/railtie'
 
 module Payment
   class Railtie < Rails::Railtie
-    config.payment_client = Struct.new(:download_directory)
+    config.payment_client = ActiveSupport::OrderedOptions.new
 
     config.after_initialize do |app|
       Payment::Client::download_directory = app.config.payment_client.download_directory
